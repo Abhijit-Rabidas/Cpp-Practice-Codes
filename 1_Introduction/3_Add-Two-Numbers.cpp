23533b122813f53af732a3fb48f83266c6317856
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-09 20:52:28 +0530

    Update 3_Add-Two-Numbers.cpp

--- 1_Introduction/3_Add-Two-Numbers.cpp
+++ 1_Introduction/3_Add-Two-Numbers.cpp
@@ -16,3 +16,20 @@ int main()
 
     cout << "sum of " << num1 << " + " << num2 << " = " << sum;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "sum of " << num1 << " + " << num2 << " = " << (num1 + num2);
+} 
+*/
