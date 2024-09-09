bfcaa6cee733aaa6ebf49828fa8c725a5756ff37
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-09 21:25:34 +0530

    Add files via upload

---
+++ 1_Introduction/8_MultiplicationOf2Nums.cpp
@@ -0,0 +1,20 @@
+// C++ Program to Multiply two Numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2, product;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    product = num1 * num2;
+
+    cout << num1 << " * " << num2 << " = " << product ;
+    return 0;
+}
