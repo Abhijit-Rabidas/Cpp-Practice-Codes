0373f4700c2fabcab38906e91c3131b48ea72a01
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-09 20:59:24 +0530

    Add files via upload

---
+++ 1_Introduction/5_SizefofDatatype.cpp
@@ -0,0 +1,15 @@
+// C++ Program to Find Size of int, float, double and char in Your System
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "size of char: " << sizeof(char) << " byte" << endl;
+
+    return 0;
+}
