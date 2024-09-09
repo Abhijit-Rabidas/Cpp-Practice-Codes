3d77eac62354e3353d80441e9fe3ff89a933d5a6
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-09 20:48:29 +0530

    Add files via upload

---
+++ 1_Introduction/3_Add-Two-Numbers.cpp
@@ -0,0 +1,18 @@
+// C++ Program to Add Two Numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "sum of " << num1 << " + " << num2 << " = " << sum;
+}
